c0fa18d6405cf7dee3ce420cfe630f3408056ba4
Audrey <audrey@spade.be>
2020-05-26 11:56:24 +0200

    update submodules

--- src/AutoComplete/init.js
+++ src/AutoComplete/init.js
@@ -1,6 +1,7 @@
 var AutoCompleteInput = require('./Core/Input')
 var AutoComplete = module.exports = {
     init: function (params) {
+        console.log(params)
         return new AutoCompleteInput().construct(params) 
     }
 }
